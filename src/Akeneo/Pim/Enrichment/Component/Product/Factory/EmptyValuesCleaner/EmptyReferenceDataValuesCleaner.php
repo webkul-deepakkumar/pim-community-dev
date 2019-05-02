8039e275ab5122589f9043066858f83e83411d45
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-02 11:49:10 +0200

    TIP-1158: Add reference data filter and cleaner

---
+++ src/Akeneo/Pim/Enrichment/Component/Product/Factory/EmptyValuesCleaner/EmptyReferenceDataValuesCleaner.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Akeneo\Pim\Enrichment\Component\Product\Factory\EmptyValuesCleaner;
+
+
+use Akeneo\Pim\Structure\Component\AttributeTypes;
+
+class EmptyReferenceDataValuesCleaner implements EmptyValuesCleaner
+{
+    public function clean(OnGoingCleanedRawValues $onGoingCleanedRawValues): OnGoingCleanedRawValues
+    {
+        $referenceDataValues = $onGoingCleanedRawValues->nonCleanedValuesOfTypes(AttributeTypes::REFERENCE_DATA_SIMPLE_SELECT, AttributeTypes::REFERENCE_DATA_SIMPLE_SELECT);
+
+
+    }
+}
---
+++ src/Akeneo/Pim/Enrichment/Component/Product/Factory/NonExistentValuesFilter/NonExistentReferenceDataValuesFilter.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Akeneo\Pim\Enrichment\Component\Product\Factory\NonExistentValuesFilter;
+
+
+class NonExistentReferenceDataValuesFilter implements NonExistentValuesFilter
+{
+    private
+
+    public function filter(OnGoingFilteredRawValues $onGoingFilteredRawValues): OnGoingFilteredRawValues
+    {
+
+    }
+}
