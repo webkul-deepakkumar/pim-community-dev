4c2b6ae0a42cfdaed25db4b723c704e1cdf1e561
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-02 15:06:24 +0200

    TIP-1158: Use query in filter

--- src/Akeneo/Pim/Enrichment/Component/Product/Factory/NonExistentValuesFilter/NonExistentReferenceDataValuesFilter.php
+++ src/Akeneo/Pim/Enrichment/Component/Product/Factory/NonExistentValuesFilter/NonExistentReferenceDataValuesFilter.php
@@ -4,12 +4,29 @@
 namespace Akeneo\Pim\Enrichment\Component\Product\Factory\NonExistentValuesFilter;
 
 
+use Akeneo\Pim\Enrichment\Bundle\Product\Query\Sql\GetExistingReferenceDataCodes;
+use Akeneo\Pim\Structure\Component\AttributeTypes;
+
 class NonExistentReferenceDataValuesFilter implements NonExistentValuesFilter
 {
-    private
+    /** @var GetExistingReferenceDataCodes */
+    private $getExistingReferenceDataCodes;
+
+    public function __construct(GetExistingReferenceDataCodes $getExistingReferenceDataCodes)
+    {
+        $this->getExistingReferenceDataCodes = $getExistingReferenceDataCodes;
+    }
 
     public function filter(OnGoingFilteredRawValues $onGoingFilteredRawValues): OnGoingFilteredRawValues
     {
+        $referenceDataValues = $onGoingFilteredRawValues->notFilteredValuesOfTypes(
+            AttributeTypes::REFERENCE_DATA_SIMPLE_SELECT,
+            AttributeTypes::REFERENCE_DATA_MULTI_SELECT
+        );
+
+        if (empty($referenceDataValues)) {
+            return $onGoingFilteredRawValues;
+        }
 
     }
 }
